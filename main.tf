4ba5428f17f9c62f5f868ca284532502095760f1
2rpadala <rajvja520002@gmail.com>
2023-05-09 17:08:02 +0530

    ssm terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "SecureString"
+resource "aws_ssm_parameter" "parameters" {
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type  = "String"
   key_id = "dd8e81e7-b522-45d1-b0ae-e02df6b2243f"
-  value = "hello parameter"
+
 }
